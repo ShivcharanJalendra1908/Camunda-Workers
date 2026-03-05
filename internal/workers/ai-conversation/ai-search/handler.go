259af38810492032cd53c170d424ab56ee753837
ShivcharanJalendra1908 <shivcharanjalendra@gmail.com>
2026-03-05 14:05:39 +0530

    fix: Ai Search

--- internal/workers/ai-conversation/ai-search/handler.go
+++ internal/workers/ai-conversation/ai-search/handler.go
@@ -358,11 +358,31 @@ func (h *Handler) buildElasticsearchQuery(params *ExtractedParameters) (map[stri
 
 	// Industry match
 	if params.Industry != "" {
+		industrySlug := strings.ToLower(params.Industry)
+		industrySlug = strings.ReplaceAll(industrySlug, " & ", " ")
+		industrySlug = strings.ReplaceAll(industrySlug, " / ", " ")
+		industrySlug = strings.ReplaceAll(industrySlug, "&", "")
+		industrySlug = strings.ReplaceAll(industrySlug, "/", "")
+		industrySlug = strings.ReplaceAll(industrySlug, " ", "-")
+
 		mustClauses = append(mustClauses, map[string]interface{}{
-			"multi_match": map[string]interface{}{
-				"query":  params.Industry,
-				"fields": []string{"industry.name^3", "industry.slug^2"},
-				"type":   "best_fields",
+			"bool": map[string]interface{}{
+				"should": []interface{}{
+					map[string]interface{}{
+						"match": map[string]interface{}{
+							"industry.name": map[string]interface{}{
+								"query": params.Industry,
+								"boost": 3,
+							},
+						},
+					},
+					map[string]interface{}{
+						"term": map[string]interface{}{
+							"industry.slug": industrySlug,
+						},
+					},
+				},
+				"minimum_should_match": 1,
 			},
 		})
 	}
