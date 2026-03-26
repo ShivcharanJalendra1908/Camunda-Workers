20f7441705844f34ebafc400a309165de303f845
ShivcharanJalendra1908 <shivcharanjalendra@gmail.com>
2026-03-27 01:47:20 +0530

    fix: Enq page

--- internal/workers/data-access/query-postgresql/queries/user.go
+++ internal/workers/data-access/query-postgresql/queries/user.go
@@ -1,49 +1,50 @@
-// internal/workers/data-access/query-postgresql/queries/user.go
-package queries
 
-import (
-	"context"
-	"database/sql"
-	"time"
-)
+// // internal/workers/data-access/query-postgresql/queries/user.go
+// package queries
 
-func UserProfile(ctx context.Context, db *sql.DB, params map[string]interface{}) (interface{}, int, int64, error) {
-	userID, ok := params["userId"].(string)
-	if !ok {
-		return nil, 0, 0, ErrMissingParam
-	}
+// import (
+// 	"context"
+// 	"database/sql"
+// 	"time"
+// )
 
-	start := time.Now()
+// func UserProfile(ctx context.Context, db *sql.DB, params map[string]interface{}) (interface{}, int, int64, error) {
+// 	userID, ok := params["userId"].(string)
+// 	if !ok {
+// 		return nil, 0, 0, ErrMissingParam
+// 	}
 
-	var id, name, email, subscriptionTier string
-	var capitalAvailable, industryExperience int
-	var locationPreferences, interests string
+// 	start := time.Now()
 
-	err := db.QueryRowContext(ctx, `
-		SELECT id, name, email, subscription_tier, capital_available, 
-		       industry_experience, location_preferences, interests
-		FROM users 
-		WHERE id = $1`, userID).Scan(
-		&id, &name, &email,
-		&subscriptionTier, &capitalAvailable,
-		&industryExperience, &locationPreferences,
-		&interests,
-	)
-	if err != nil {
-		return nil, 0, 0, err
-	}
+// 	var id, name, email, subscriptionTier string
+// 	var capitalAvailable, industryExperience int
+// 	var locationPreferences, interests string
 
-	result := map[string]interface{}{
-		"id":                  id,
-		"name":                name,
-		"email":               email,
-		"subscriptionTier":    subscriptionTier,
-		"capitalAvailable":    capitalAvailable,
-		"industryExperience":  industryExperience,
-		"locationPreferences": locationPreferences,
-		"interests":           interests,
-	}
+// 	err := db.QueryRowContext(ctx, `
+// 		SELECT id, name, email, subscription_tier, capital_available, 
+// 		       industry_experience, location_preferences, interests
+// 		FROM users 
+// 		WHERE id = $1`, userID).Scan(
+// 		&id, &name, &email,
+// 		&subscriptionTier, &capitalAvailable,
+// 		&industryExperience, &locationPreferences,
+// 		&interests,
+// 	)
+// 	if err != nil {
+// 		return nil, 0, 0, err
+// 	}
 
-	execTime := time.Since(start).Milliseconds()
-	return result, 1, execTime, nil
-}
+// 	result := map[string]interface{}{
+// 		"id":                  id,
+// 		"name":                name,
+// 		"email":               email,
+// 		"subscriptionTier":    subscriptionTier,
+// 		"capitalAvailable":    capitalAvailable,
+// 		"industryExperience":  industryExperience,
+// 		"locationPreferences": locationPreferences,
+// 		"interests":           interests,
+// 	}
+
+// 	execTime := time.Since(start).Milliseconds()
+// 	return result, 1, execTime, nil
+// }
